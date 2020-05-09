3d5904b63eb3dc0a7b3e5ffe25d3c5a7dfbfd730
xwen99 <im.xwen@gmail.com>
2020-05-09 10:26:20 +0800

    predict after train

--- with_paddle_hub/paddlehub_reading_comprehension.sh
+++ with_paddle_hub/paddlehub_reading_comprehension.sh
@@ -1,16 +1,16 @@
 export FLAGS_eager_delete_tensor_gb=0.0
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 DATASET_PATH="../data"
 
 python -u reading_comprehension.py \
                    --dataset_path=${DATASET_PATH} \
-                   --batch_size=8 \
+                   --batch_size=32 \
                    --use_gpu=True \
                    --checkpoint_dir="./ckpt_dureader" \
-                   --learning_rate=3e-5 \
+                   --learning_rate=1e-6 \
                    --weight_decay=0.01 \
                    --warmup_proportion=0.1 \
                    --num_epoch=5 \
                    --max_seq_len=512 \
-                   --use_data_parallel=False
+                   --use_data_parallel=True
